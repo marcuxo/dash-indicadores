e88863db9b2615be13b794c347af569315fc901a
Marco Urrutia <marcourrutiam@gmail.com>
2021-11-03 17:11:05 -0300

    make a beutifull methods

--- src/component/chart/mainchart.js
+++ src/component/chart/mainchart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
 import {url} from '../../url/url.link'
-
+/** Marcuxo */
 function Mainchart({titulo,token}) {
   const [categoria, setCategoria] = useState(['Cargando'])
   const [serie, setSerie] = useState([{name:"",data:[0]}])
